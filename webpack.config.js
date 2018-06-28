2324e0a8cc13e22cc6f82b71a182edce4be5a747
DamirKakharov <k.damir97@gmail.com>
2018-06-28 12:10:32 +0600

    Конфиг Webpakc'a

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html",
+        }),
+    ],
+    performance: { hints: false },
+};
